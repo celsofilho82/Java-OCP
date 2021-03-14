b5eae85c00f59f89c07e7b1b0e238480af3b69a5
Celso Ribeiro <celsofilho82@gmail.com>
2021-03-14 16:46:03 -0300

    Practice 6-3: Create Factory Methods Previous

--- ProductManagement/src/labs/pm/app/Shop.java
+++ ProductManagement/src/labs/pm/app/Shop.java
@@ -3,20 +3,20 @@ package labs.pm.app;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import labs.pm.data.Drink;
-import labs.pm.data.Food;
 import labs.pm.data.Product;
+import labs.pm.data.ProductManager;
 import labs.pm.data.Rating;
 
 public class Shop {
 
 	public static void main(String[] args) {
-		Product p1 = new Drink(101, "Tea", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
-		Product p2 = new Drink(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
-		Product p3 = new Food(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
+		ProductManager manager = new ProductManager();
+		Product p1 = manager.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
+		Product p2 = manager.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
+		Product p3 = manager.createProduct(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
 		Product p5 = p3.applyRating(Rating.THREE_STAR);
-		Product p6 = new Drink(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR);
-		Product p7 = new Food(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR,
+		Product p6 = manager.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR);
+		Product p7 = manager.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR,
 				LocalDate.now().plusDays(2));
 		Product p8 = p2.applyRating(Rating.FIVE_STAR);
 		Product p9 = p1.applyRating(Rating.TWO_STAR);
--- ProductManagement/src/labs/pm/data/Product.java
+++ ProductManagement/src/labs/pm/data/Product.java
@@ -14,18 +14,18 @@ public abstract class Product {
 	private BigDecimal price;
 	private Rating rating;
 
-	public Product() {
+	Product() {
 		this(0, "no name", BigDecimal.ZERO);
 	}
 
-	public Product(int id, String name, BigDecimal price, Rating rating) {
+	Product(int id, String name, BigDecimal price, Rating rating) {
 		this.id = id;
 		this.name = name;
 		this.price = price;
 		this.rating = rating;
 	}
 
-	public Product(int id, String name, BigDecimal price) {
+	Product(int id, String name, BigDecimal price) {
 		this.id = id;
 		this.name = name;
 		this.price = price;
