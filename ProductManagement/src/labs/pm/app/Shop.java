2cf459d52b8db887f808d04a280f30a1703f62d3
Celso Ribeiro <celsofilho82@gmail.com>
2021-03-21 13:35:16 -0300

    Practice 9-1: Organize Products and Reviews into a HashMap

--- ProductManagement/src/labs/pm/app/Shop.java
+++ ProductManagement/src/labs/pm/app/Shop.java
@@ -13,31 +13,31 @@ public class Shop {
 	public static void main(String[] args) {
 		ProductManager manager = new ProductManager(new Locale("pt", "BR"));
 		Product p1 = manager.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.NOT_RATED);
-		manager.printProductReport();
+		manager.printProductReport(p1);
 		p1 = manager.reviewProduct(p1, Rating.FOUR_STAR, "Nice hot cup of tea");
 		p1 = manager.reviewProduct(p1, Rating.TWO_STAR, "Nice hot cup of tea");
-		p1 = manager.reviewProduct(p1, Rating.THREE_STAR, "Nice hot cup of tea");
-		p1 = manager.reviewProduct(p1, Rating.FIVE_STAR, "Nice hot cup of tea");
-		p1 = manager.reviewProduct(p1, Rating.FOUR_STAR, "Nice hot cup of tea");
-		p1 = manager.reviewProduct(p1, Rating.THREE_STAR, "Nice hot cup of tea");
-		
-		manager.printProductReport();
-//		Product p2 = manager.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
-//		Product p3 = manager.createProduct(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
-//		Product p5 = p3.applyRating(Rating.THREE_STAR);
-//		Product p6 = manager.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR);
-//		Product p7 = manager.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR,
-//				LocalDate.now().plusDays(2));
-//		Product p8 = p2.applyRating(Rating.FIVE_STAR);
-//		Product p9 = p1.applyRating(Rating.TWO_STAR);
-//
-//		System.out.println(p1);
-//		System.out.println(p2);
-//		System.out.println(p3);
-//		System.out.println(p5);
-//		System.out.println(p8);
-//		System.out.println(p9);
-//		System.out.println(p6.equals(p7));
+		p1 = manager.reviewProduct(p1, Rating.THREE_STAR, "Nice hot cup of tea");	
+		manager.printProductReport(p1);
+
+		Product p2 = manager.createProduct(102, "Coffee", BigDecimal.valueOf(2.99), Rating.NOT_RATED);
+		p2 = manager.reviewProduct(p2, Rating.THREE_STAR, "Coffee was ok");
+		p2 = manager.reviewProduct(p2, Rating.ONE_STAR, "Coffee was ok");
+		p2 = manager.reviewProduct(p2, Rating.FIVE_STAR, "Coffee was ok");
+		manager.printProductReport(p2);
+
+		Product p21 = manager.createProduct(103, "Cake", BigDecimal.valueOf(3.99), Rating.NOT_RATED,
+				LocalDate.now().plusDays(2));
+		p21 = manager.reviewProduct(p21, Rating.THREE_STAR, "Cake was ok");
+		p21 = manager.reviewProduct(p21, Rating.ONE_STAR, "Cake was ok");
+		p21 = manager.reviewProduct(p21, Rating.FIVE_STAR, "Cake was ok");
+		manager.printProductReport(p21);
+
+		Product p211 = manager.createProduct(104, "Chocolate", BigDecimal.valueOf(2.50), Rating.NOT_RATED,
+				LocalDate.now().plusDays(3));
+		p211 = manager.reviewProduct(p211, Rating.THREE_STAR, "Chocolate was ok");
+		p211 = manager.reviewProduct(p211, Rating.ONE_STAR, "Chocolate was ok");
+		p211 = manager.reviewProduct(p211, Rating.FIVE_STAR, "Chocolate was ok");
+		manager.printProductReport(p211);
 	}
 
 }
